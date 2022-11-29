50c7b680f04576ec731ff6b231c4d0bd97b5d2f1
Elinor <elinorco24@gmail.com>
2022-11-29 21:07:49 +0200

    Uptade DB creation script

--- database/DataBase functionality.js
+++ database/DataBase functionality.js
@@ -4,7 +4,7 @@ const check_connection = async (con) => con.connect((err) => {
 });
 
 
-const authentication = async (con, email, password) => {
+const authentication_login = async (con, email, password) => {
 
     let sql_query = `SELECT email FROM users_details WHERE email = ? AND password = ?`;
 
@@ -25,7 +25,7 @@ const authentication = async (con, email, password) => {
 }
 
 
-const check_user = async (con, email) => {
+const check_email = async (con, email) => {
 
     let sql_query = `SELECT email FROM users_details WHERE email = ?`;
 
@@ -46,38 +46,87 @@ const check_user = async (con, email) => {
 }
 
 
-const insert_user = async (con, email, first_name, last_name, phone_number, password, creation_token, activated) => {
+//don't export
+const delete_earliest_password = async (con, email) => {
+    let sql_query_delete = `with tbl as(select * from communication_ltd.password_history)
+    delete from communication_ltd.password_history where 
+    email = ? and creation_date <= all(select tbl.creation_date from tbl)`;
+    con.query(sql_query_delete, [email], (err, result) => {
+        if (err) {
+            return reject(err);
+        }
+        else {
+            console.log(result);
+        }
+    })
+
+}
+
+
+//don't export
+const update_pass_history = async (con, email, password) => {
+
+    let sql_count_query = `SELECT count(email) AS passNum FROM communication_ltd.password_history WHERE email=?`;
+
+    let sql_add_latest = `INSERT INTO communication_ltd.password_history (email,password,creation_date) VALUES (?, ?, NOW())`;
+
+    con.query(sql_count_query, [email], async (err, result) => {
+        if (err) {
+            return reject(err);
+        }
+        else if (result[0].passNum >= 3) {
+            await delete_earliest_password(con, email);
+            con.query(sql_add_latest, [email, password], async (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                else {
+                    console.log("Password updated...");
+                    return;
+                }
+            })
+            return;
+        }
+        else {
+            con.query(sql_add_latest, [email, password], (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                else {
+                    console.log("Added password...");
+                    return;
+                }
+            })
+            return;
+        }
+    })
+}
+
+
+const insert_user = async (con, email, first_name, last_name, phone_number, password, creation_token) => {
 
     let sql_query_users = `INSERT INTO communication_ltd.users_details (email,first_name,last_name,phone_number,password,creation_token, activated) VALUES (?, ?, ?, ?, ?, ?, 0)`;
 
-    let sql_query_passwords = `INSERT INTO communication_ltd.password_history (email,password,creation_date) VALUES (?, ?, NOW())`;
+    if (await check_email(con, email)) {
+        console.log("Already signed up....");
+        return;
+    }
 
-    return new Promise((resolve, reject) => {
-        con.query(sql_query_users, [email, first_name, last_name, phone_number, password, creation_token], (err, result) => {
-            // if (err) {
-            //     return reject(err);
-            // }
-            if (check_user(con, email)) {
-                return resolve("Already signed up....");
-            }
-            else {
-                resolve("Added user...")
-            }
+    else {
+        return new Promise(async (resolve, reject) => {
+            con.query(sql_query_users, [email, first_name, last_name, phone_number, password, creation_token], async (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                else {
+                    console.log("Added user...");
+                    resolve("Added user...");
+                }
+            });
+            await update_pass_history(con, email, password);
         });
-
-        // con.query(sql_query_passwords, [email, password], (err, result) => {
-        //     // if (err) {
-        //     //     return reject(err);
-        //     // }
-        //     if (check_user(con, email)) {
-        //         return reject("Already signed up....");
-        //     }
-        //     else {
-        //         resolve("Added password...")
-        //     }
-        // });
-
-    });
+    }
 }
 
 
@@ -110,7 +159,7 @@ const delete_user = async (con, email) => {
 
 
 
-export { authentication, check_user, check_connection, insert_user, delete_user }
+export { authentication_login, check_email, check_connection, insert_user, delete_user }
 
 
 
