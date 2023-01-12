68193ca1235718e076b73683b22491c534c1a271
BarSalem <barsalem555@gmail.com>
2023-01-12 19:48:18 +0200

    Database (#20)
    * add secure functions and edit the server and the configuration files
    
    Co-authored-by: Elinor <elinorco24@gmail.com>

--- database/DB_Connect.js
+++ database/DB_Connect.js
@@ -1,8 +1,24 @@
-import config from '../configuration.json' assert {type: 'json'};
-import mysql from 'mysql'
-import { check_connection, authentication_login, check_email, insert_user, delete_user, update_password, insert_client, delete_client, get_all_clients, sort_by, search, activate_user, forgot_pass } from './DataBase functionality.js'
+import config from "../configuration.json" assert { type: "json" };
+import mysql from "mysql";
+import {
+  check_connection,
+  authentication_login,
+  check_user_email,
+  check_client_email,
+  insert_user,
+  delete_user,
+  update_password,
+  insert_client,
+  delete_client,
+  get_all_clients,
+  sort_by,
+  search,
+  activate_user,
+  forgot_pass,
+  check_login_attempts
+} from "./DataBase_functionality.js";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -13,11 +29,51 @@ var connection = mysql.createConnection({
 
 //check_connection(connection)
 //delete_user(connection, "Amit1@gmail.com")
-//await insert_user(connection, "Amit@gmail.com", "Amit", "Amazing", "0525555443", "Amit100", "657657657657");
+// await insert_user(
+//   connection,
+//   "Tomer@gmail.com",
+//   "Tomer",
+//   "Haina",
+//   "0525555443",
+//   "Tomer100",
+//   "657657657657"
+// );
+// await insert_user(
+//   connection,
+//   "Amit@gmail.com",
+//   "Amit",
+//   "Amazing",
+//   "0525555443",
+//   "Tomer100",
+//   "657657657657"
+// );
+// await insert_user(
+//   connection,
+//   "Bar@gmail.com",
+//   "Bar",
+//   "Salem",
+//   "0525555443",
+//   "Tomer100",
+//   "657657657657"
+// );
 //await authentication_login(connection, "Nati@gmail.com", "123456");
 //await update_password(connection, "Amit1@gmail.com", "Amit102");
-//await insert_client(connection, "Bar@gmail.com", "Bar", "Salem", "0525381648", "Holon");
-//await insert_client(connection, "Eli@gmail.com", "Eli", "Cohen", "0525881648", "Yehud");
+// await insert_client(
+//   connection,
+//   "Bar@gmail.com",
+//   "Bar",
+//   "Salem",
+//   "0525381648",
+//   "Holon"
+// );
+// await insert_client(
+//   connection,
+//   "Eli@gmail.com",
+//   "Eli",
+//   "Cohen",
+//   "0525881648",
+//   "Yehud"
+// );
 //await delete_client(connection, "Bar@gmail.com");
 // console.log(await get_all_clients(connection, 0));
 // console.log(await sort_by(connection, config.DB.tables.clients.fields.last_name));
--- database/DB_Creation.js
+++ database/DB_Creation.js
@@ -1,55 +1,61 @@
-import config from '../configuration.json' assert {type: 'json'};
-import mysql from './mysql'
-
+import config from "../configuration.json" assert { type: "json" };
+import mysql from "mysql";
+import dotenv from "dotenv";
+dotenv.config();
 
 var con = mysql.createConnection({
-    host: config.DB.host,
-    user: config.DB.user,
-    password: config.DB.password
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 });
 
 con.connect((err) => {
-    if (err) throw err;
-    console.log("Connected!");
-    con.end()
+  if (err) throw err;
+  console.log("Connected!");
+  con.end();
 });
 
-con.query("CREATE DATABASE IF NOT EXISTS " + config.DB.db_name, (err, result) => {
+con.query(
+  "CREATE DATABASE IF NOT EXISTS " + process.env.DB_NAME,
+  (err, result) => {
     if (err) throw err;
     console.log("Database created");
+  }
+);
+
+con.changeUser({ database: process.env.DB_NAME }, (err) => {
+  if (err) throw err;
 });
 
-con.changeUser({ database: config.DB.db_name }, (err) => {
-    if (err) throw err;
-});
-
-
-let sql_users_table = "CREATE TABLE IF NOT EXISTS " + config.DB.db_name + "." + config.DB.tables.users_details.table_name +
-    "(`email` VARCHAR(50) NOT NULL, `first_name` VARCHAR(45) NOT NULL, `last_name` VARCHAR(45) NOT NULL, `phone_number` VARCHAR(10) NOT NULL, `password` VARCHAR(45) NOT NULL, `creation_token` VARCHAR(45) NOT NULL, `activated` TINYINT NOT NULL, PRIMARY KEY(`email`))"
-
+let sql_users_table =
+  "CREATE TABLE IF NOT EXISTS " +
+  process.env.DB_NAME +
+  ".users_details" +
+  "(`email` VARCHAR(50) NOT NULL, `first_name` VARCHAR(45) NOT NULL, `last_name` VARCHAR(45) NOT NULL, `phone_number` VARCHAR(10) NOT NULL, `password` VARCHAR(128) NOT NULL, `password_token` VARCHAR(255) NOT NULL, `pass_token_activated` TINYINT NOT NULL , `creation_token` VARCHAR(45) NOT NULL, `logins` INT NOT NULL, `login_time`DATETIME NOT NULL, `activated` TINYINT NOT NULL, PRIMARY KEY(`email`))";
 
 con.query(sql_users_table, function (err, result) {
-    if (err) throw err;
-    console.log("Users table created");
+  if (err) throw err;
+  console.log("Users table created");
 });
 
-
-
-let sql_history_table = "CREATE TABLE IF NOT EXISTS " + config.DB.db_name + "." + config.DB.tables.password_history.table_name +
-    "(`email` VARCHAR(50) NOT NULL, `password` VARCHAR(45) NOT NULL,`creation_date` DATETIME NOT NULL, PRIMARY KEY(`email`, `password`))"
-
+let sql_history_table =
+  "CREATE TABLE IF NOT EXISTS " +
+  process.env.DB_NAME +
+  ".password_history" +
+  "(`email` VARCHAR(50) NOT NULL, `password` VARCHAR(45) NOT NULL,`creation_date` DATETIME NOT NULL, PRIMARY KEY(`email`, `password`))";
 
 con.query(sql_history_table, function (err, result) {
-    if (err) throw err;
-    console.log("Password history table created");
+  if (err) throw err;
+  console.log("Password history table created");
 });
 
-
-let sql_clients_table = "CREATE TABLE IF NOT EXISTS " + config.DB.db_name + "." + config.DB.tables.clients.table_name +
-    "(`email` VARCHAR(50) NOT NULL, `first_name` VARCHAR(45) NOT NULL, `last_name` VARCHAR(45) NOT NULL, `phone_number` VARCHAR(10) NOT NULL, `city` VARCHAR(45) NOT NULL, PRIMARY KEY(`email`))"
-
+let sql_clients_table =
+  "CREATE TABLE IF NOT EXISTS " +
+  process.env.DB_NAME +
+  ".clients" +
+  "(`email` VARCHAR(50) NOT NULL, `first_name` VARCHAR(45) NOT NULL, `last_name` VARCHAR(45) NOT NULL, `phone_number` VARCHAR(10) NOT NULL, `city` VARCHAR(45) NOT NULL, PRIMARY KEY(`email`))";
 
 con.query(sql_clients_table, function (err, result) {
-    if (err) throw err;
-    console.log("Clients table created");
+  if (err) throw err;
+  console.log("Clients table created");
 });
