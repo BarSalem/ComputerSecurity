4114b260126c236ffe6cb60d1447ab7c86baef64
BarSalem <barsalem555@gmail.com>
2023-01-12 20:31:23 +0200

    Server dev (#18) (#21)
    * Fixing routes with DB
    
    * Adding clients to the table
    
    * Adding more connections

--- database/DB_Connect.js
+++ database/DB_Connect.js
@@ -20,7 +20,7 @@ import {
 import dotenv from "dotenv";
 dotenv.config();
 
-var connection = mysql.createConnection({
+export var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
