de36a0b86b7918b359650845b5ef29869f8355f4
BarSalem <barsalem555@gmail.com>
2023-01-13 16:02:19 +0200

    Adding SSL and TLS connection

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import https from 'https';
 
 import fs from 'fs';
 import tls from 'tls';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { sendChangePasswordName, sendConfirmationEmail } from '../server/node_mailing.js'
-import { connection } from '../database/DB_Connect.js'
+import { sendChangePasswordName, sendConfirmationEmail } from '../server/node_mailing.js';
+import { connection } from '../database/DB_Connect.js';
 import { hashPassword, generateRandomString } from "./encryption.js";
 import { checkPassword } from "../security/secure_functions.js";
 import {   check_connection,get_user_name,
@@ -26,7 +27,7 @@ import {   check_connection,get_user_name,
     search,
     activate_user,
     forgot_pass,
-    update_password_token } from '../database/DataBase_functionality.js'
+    update_password_token } from '../database/DataBase_functionality.js';
 
 
 const app = express();
@@ -37,88 +38,89 @@ const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, '/../front')));
 
 const options = {
-    key: fs.readFileSync(path.join(__dirname,'localhost.key')),
-    cert: fs.readFileSync(path.join(__dirname,'./localhost.crt')),
+    port: 443,
+    key: fs.readFileSync(path.join(__dirname,'localhost-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname,'localhost.pem')),
     minVersion: tls.Server.TLSv1_2_method
   };
 
 app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/../front/login-page.html'));
-})
+});
 
 app.get('/info', async (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/../front/table-view.html'));
-})
+});
 
 app.get('/register', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/../front/registration-page.html'));
-})
+});
 
 app.get('/forgotpassword', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/../front/forgot-password.html'));
-})
+});
 
 app.get('/changepassword:id', (req, res) => {
     // activate password token of user
     res.status(200).sendFile(path.join(__dirname + '/../front/change-password.html'));
-})
+});
 
 app.get('/activationsuccess', async (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/../front/activation-page.html'));
-})
+});
 
 app.get('/activation/:id', async (req, res) => {
-    const activatedSuccessed = await activate_user(connection, req.params.id)
-    console.log(activatedSuccessed)
+    const activatedSuccessed = await activate_user(connection, req.params.id);
+    console.log(activatedSuccessed);
     if (activatedSuccessed) {
         res.status(200).redirect('/activationsuccess');
     }
     else {
         res.status(404).send({ "message": "Something went wrong, please try again later!" });
     }
-})
+});
 
 app.post('/register', async (req, res) => {
-    const {fname, lname, user_email, user_password, user_phone} = req.body
-    console.log(user_email, fname, lname, user_password, user_phone)
-    const is_valid_password = checkPassword(user_password)
+    const {fname, lname, user_email, user_password, user_phone} = req.body;
+    console.log(user_email, fname, lname, user_password, user_phone);
+    const is_valid_password = checkPassword(user_password);
     if(is_valid_password === 'all required elements'){
-    const hashed_password = hashPassword(user_password)
-    const user_token = generateRandomString()
-    const create_user_status = await insert_user(connection, user_email, fname, lname, user_phone, hashed_password, user_token)
-    console.log(create_user_status)
+    const hashed_password = hashPassword(user_password);
+    const user_token = generateRandomString();
+    const create_user_status = await insert_user(connection, user_email, fname, lname, user_phone, hashed_password, user_token);
+    console.log(create_user_status);
     if (create_user_status) {
-        sendConfirmationEmail(fname, user_email, user_token)
-        res.status(200).send({result: 'redirect', url:'/', message:"Go activate your account in the email"})
+        sendConfirmationEmail(fname, user_email, user_token);
+        res.status(200).send({result: 'redirect', url:'/', message:"Go activate your account in the email"});
     }
     else {
         res.status(200).send({message:"This email is already used"});
     }}
     else{
-        res.status(200).send({result: 'redirect', url:'/', error: is_valid_password})
+        res.status(200).send({result: 'redirect', url:'/', error: is_valid_password});
     }
-})
+});
 
 app.post('/forgot-password', async (req, res) => {
-    const user_email = req.body.user_email
-    const user_email_exist = await check_user_email(connection, user_email)
+    const user_email = req.body.user_email;
+    const user_email_exist = await check_user_email(connection, user_email);
     if (user_email_exist) {
-        const user_password_token = generateRandomString()
-        const forgot_password_succ = await forgot_pass(connection, user_email, user_password_token)
+        const user_password_token = generateRandomString();
+        const forgot_password_succ = await forgot_pass(connection, user_email, user_password_token);
         if (forgot_password_succ){
-            sendChangePasswordName(user_email, user_password_token)
+            sendChangePasswordName(user_email, user_password_token);
         }
     }
     res.status(200).send({ "message": "If the user exist, the mail has been sent!" });
-})
+});
 
 app.post('/change-password', async (req, res) => {
-    const token = req.body.token
-    const newPassword = req.body.new_password
-    const new_hashed_password = hashPassword(newPassword)
-    const is_valid_password = checkPassword(newPassword)
+    const token = req.body.token;
+    const newPassword = req.body.new_password;
+    const new_hashed_password = hashPassword(newPassword);
+    const is_valid_password = checkPassword(newPassword);
     if(is_valid_password === 'all required elements'){
-    const user_email_exist = await update_password_token(connection, new_hashed_password, token)
+    const user_email_exist = await update_password_token(connection, new_hashed_password, token);
     console.log(user_email_exist);
     if (user_email_exist) {
         res.status(200).send({result: 'redirect', url:'/', message:"Please log-in with the new password"});}
@@ -127,53 +129,53 @@ app.post('/change-password', async (req, res) => {
         }
     }
     else{
-        res.status(200).send({error: is_valid_password})
+        res.status(200).send({error: is_valid_password});
     }
-    })
+    });
 
 app.post('/login', async (req, res) => {
-    const user_email = req.body.user_email
-    const user_password = req.body.password
-        const hashed_password = hashPassword(user_password)
-        const login_user_status = await authentication_login(connection, user_email, hashed_password)
-        if (login_user_status) {
-        const user_name = await get_user_name(connection,user_email)
-        res.status(200).send({result: 'redirect', url:'/info', name: user_name})
+    const user_email = req.body.user_email;
+    const user_password = req.body.password;
+    const hashed_password = hashPassword(user_password);
+    const login_user_status = await authentication_login(connection, user_email, hashed_password);
+    if (login_user_status) {
+        const user_name = await get_user_name(connection,user_email);
+        res.status(200).send({result: 'redirect', url:'/info', name: user_name});
         }
         else {
         res.status(200).send({error: "Wrong credentials"})
         }
-})
+});
 
 app.post('/add-client', async (req, res) => {
     const {email, fname, lname, phone,city} = req.body;
-    const insert_client_status = await insert_client(connection, email, fname, lname, phone,city)
-    console.log(insert_client_status)
+    const insert_client_status = await insert_client(connection, email, fname, lname, phone,city);
+    console.log(insert_client_status);
     if (insert_client_status) {
         res.status(200).send({result: 'redirect', url:'/info'});
     }
     else {
         res.status(200).redirect('/');
     }
-})
+});
 
 app.post('/del-client', async (req, res) => {
-    const insert_client_status = await delete_client(connection, req.body.email)
+    const insert_client_status = await delete_client(connection, req.body.email);
     if (insert_client_status) {
         res.status(200).send({result: 'redirect', url:'/info'});
     }
     else {
         res.status(200).redirect('/');
     }
-})
+});
 
 app.post('/changepasswordlogged', async (req, res) => {
-    const {email, user_old_password, user_new_password} = req.body
-    const hashed_old_password = hashPassword(user_old_password)
-    const hashed_new_password = hashPassword(user_new_password)
-    const is_valid_password = checkPassword(user_new_password)
+    const {email, user_old_password, user_new_password} = req.body;
+    const hashed_old_password = hashPassword(user_old_password);
+    const hashed_new_password = hashPassword(user_new_password);
+    const is_valid_password = checkPassword(user_new_password);
     if(is_valid_password === 'all required elements'){
-        const insert_client_status = await update_password(connection, email, hashed_old_password, hashed_new_password)
+        const insert_client_status = await update_password(connection, email, hashed_old_password, hashed_new_password);
         if (insert_client_status) {
             res.status(200).send({result: 'redirect', url:'/', message:'If the credentials correct your password has been changed'});
         }
@@ -184,25 +186,25 @@ app.post('/changepasswordlogged', async (req, res) => {
     else {
         res.status(200).send({result: 'redirect', url:'/', error: is_valid_password})
     }
-})
+});
 
 app.post('/searchclient', async (req,res) =>{
     console.log(req.body.search_string);
     const deleted_user = await delete_user(connection, req.body.email);
     if(deleted_user) res.status(200).send({result: 'redirect', url:'/', message:'If the credentials correct your password has been changed'});
-})
+});
 
 app.get('/getclients', async (req, res) => {
-        const all_clients= await get_all_clients(connection, 0)
-        console.log(all_clients)
+        const all_clients= await get_all_clients(connection, 0);
+        console.log(all_clients);
         res.status(200).send(all_clients);
-})
+});
 
 app.post('/temp', async (req,res) =>{
     const deleted_user = await delete_user(connection, req.body.email);
     if(deleted_user) res.status(200).send({result: 'redirect', url:'/', message:'If the credentials correct your password has been changed'});
-})
+});
 
-//const server = tls.createServer(options, app);
+const server = https.createServer(options, app);
 
-app.listen(process.env.PORT,() => { console.log("Server is running on port " + process.env.PORT); })
+server.listen(process.env.PORT,() => { console.log("Server is running on port " + process.env.PORT); });
